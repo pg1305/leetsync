a678cecbcb368c88481009a2b9ec5ea7c8dbd9c9
pg1305 <135537396+pg1305@users.noreply.github.com>
2025-06-23 12:56:06 +0530

    Time: 15 ms (93.88%) | Memory: 23.9 MB (67.36%) - LeetSync

---
+++ 30-substring-with-concatenation-of-all-words/substring-with-concatenation-of-all-words.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    vector<int> findSubstring(string s, vector<string>& words) {
+        vector<int> result;
+        if (s.empty() || words.empty()) return result;
+
+        int wordLen = words[0].size();
+        int wordCount = words.size();
+        int totalLen = wordLen * wordCount;
+
+        unordered_map<string, int> wordFreq;
+        for (const string &word : words)
+            wordFreq[word]++;
+
+        for (int i = 0; i < wordLen; ++i) {
+            int left = i, count = 0;
+            unordered_map<string, int> window;
+
+            for (int j = i; j + wordLen <= s.size(); j += wordLen) {
+                string word = s.substr(j, wordLen);
+
+                if (wordFreq.find(word) != wordFreq.end()) {
+                    window[word]++;
+                    count++;
+
+                    while (window[word] > wordFreq[word]) {
+                        string leftWord = s.substr(left, wordLen);
+                        window[leftWord]--;
+                        left += wordLen;
+                        count--;
+                    }
+
+                    if (count == wordCount)
+                        result.push_back(left);
+                } else {
+                    window.clear();
+                    count = 0;
+                    left = j + wordLen;
+                }
+            }
+        }
+
+        return result;
+    }
+};
