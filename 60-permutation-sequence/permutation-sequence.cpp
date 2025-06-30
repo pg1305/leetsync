38e2fbdc9527dea7001a6fd71f6dd87932c8a507
pg1305 <135537396+pg1305@users.noreply.github.com>
2025-07-01 02:18:44 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (88.02%) - LeetSync

--- 60-permutation-sequence/permutation-sequence.cpp
+++ 60-permutation-sequence/permutation-sequence.cpp
@@ -1,26 +1,28 @@
 class Solution {
 public:
+    
+    int fact(int n) {
+        int res = 1;
+        for(int i = 1; i <= n; i++) res *= i;
+        return res;
+    }
+
     string getPermutation(int n, int k) {
-        vector<int> numbers;
-        for (int i = 1; i <= n; ++i) {
-            numbers.push_back(i);
+        string nums = "";        
+        for(int i = 1; i <= n; i++)
+            nums += (i + '0');
+
+        string ans = "";
+        k--; 
+
+        for(int i = 0; i < n; i++) {
+            int f = fact(n - i - 1);      
+            int index = k / f;            
+            ans += nums[index];           
+            nums.erase(index, 1);   
+            k %= f;                       
         }
 
-        vector<int> factorial(n, 1);
-        for (int i = 1; i < n; ++i) {
-            factorial[i] = factorial[i - 1] * i;
-        }
-
-        --k;
-        string result;
-
-        for (int i = n; i >= 1; --i) {
-            int idx = k / factorial[i - 1];
-            result += to_string(numbers[idx]);
-            numbers.erase(numbers.begin() + idx);
-            k %= factorial[i - 1];
-        }
-
-        return result;
+        return ans;
     }
 };
