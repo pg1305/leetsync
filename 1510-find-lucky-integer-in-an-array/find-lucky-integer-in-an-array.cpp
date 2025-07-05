4ffd34c045e1ae8ec2b0f7075deeef082a9c84a2
pg1305 <135537396+pg1305@users.noreply.github.com>
2025-07-05 16:05:19 +0530

    Time: 0 ms (100.00%) | Memory: 13.9 MB (37.49%) - LeetSync

---
+++ 1510-find-lucky-integer-in-an-array/find-lucky-integer-in-an-array.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int findLucky(vector<int>& arr) {
+        unordered_map <int,int> freq;
+        for(int num : arr){
+            freq[num] ++;
+        }
+        int res = -1;
+        for(auto & [num,count] : freq){
+            if(num == count){
+                res = max (res,count);
+            }
+        }
+        return res;
+    }
+};
