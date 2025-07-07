2e0953ba75c5f4ac111f7682d575ee1de783dd67
pg1305 <135537396+pg1305@users.noreply.github.com>
2025-07-07 11:52:22 +0530

    Time: 59 ms (79.86%) | Memory: 74.8 MB (85.96%) - LeetSync

---
+++ 1478-maximum-number-of-events-that-can-be-attended/maximum-number-of-events-that-can-be-attended.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+
+int maxEvents(vector<vector<int>>& events) {
+    sort(events.begin(), events.end());
+
+    int n = events.size();
+    int day = 1, i = 0, count = 0;
+
+    priority_queue<int, vector<int>, greater<int>> minHeap;
+
+    int last_day = 0;
+    for (const auto& e : events) {
+        last_day = max(last_day, e[1]);
+    }
+
+    for (day = 1; day <= last_day; day++) {
+        while (i < n && events[i][0] == day) {
+            minHeap.push(events[i][1]);
+            i++;
+        }
+
+        while (!minHeap.empty() && minHeap.top() < day) {
+            minHeap.pop();
+        }
+        if (!minHeap.empty()) {
+            minHeap.pop();
+            count++;
+        }
+    }
+
+    return count;
+}
+
+};
