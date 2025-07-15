dc2f0e326ae8460b5217a39c25ecb6f91917c310
pg1305 <135537396+pg1305@users.noreply.github.com>
2025-07-15 09:23:30 +0530

    Time: 3 ms (11.41%) | Memory: 8 MB (33.01%) - LeetSync

--- 3396-valid-word/valid-word.cpp
+++ 3396-valid-word/valid-word.cpp
@@ -1,23 +1,25 @@
 class Solution {
 public:
     bool isValid(string word) {
-        if (word.size() < 3) {
+        
+        if(word.size() < 3)
             return false;
-        }
-        bool has_vowel = false;
-        bool has_consonant = false;
-        for (auto c : word) {
-            if (isalpha(c)) {
-                c = tolower(c);
-                if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
-                    has_vowel = true;
-                } else {
-                    has_consonant = true;
-                }
-            } else if (!isdigit(c)) {
-                return false;
+        
+        bool vowel = false, consonant = false;
+
+        for(char c : word)
+            if( ('0' <= c && c <= '9') || ('a' <= c && c <= 'z') || ('A' <= c && c <= 'Z') ) {
+                if(('a' <= c && c <= 'z') || ('A' <= c && c <= 'Z'))
+                    if(c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u')
+                        vowel = true;
+                    else if(c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U')
+                        vowel = true;
+                    else
+                        consonant = true;
             }
-        }
-        return has_vowel && has_consonant;
+            else
+                return false;
+        
+        return vowel && consonant;
     }
 };
