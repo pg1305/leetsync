217916cebebbee6823b4b33801393357cd260b9f
pg1305 <135537396+pg1305@users.noreply.github.com>
2025-07-16 22:06:22 +0530

    Time: 5 ms (59.12%) | Memory: 96.4 MB (27.06%) - LeetSync

--- 3490-find-the-maximum-length-of-valid-subsequence-i/find-the-maximum-length-of-valid-subsequence-i.cpp
+++ 3490-find-the-maximum-length-of-valid-subsequence-i/find-the-maximum-length-of-valid-subsequence-i.cpp
@@ -1,31 +1,13 @@
-// class Solution {
-// public:
-//     int maximumLength(vector<int>& nums) {
-//         vector<int> same(2,0) , diff(2,0);
-//         int n = nums.size();
-//         for(int i = 0; i < n; i++){
-//             int currRem = nums[i]%2;
-//             same[currRem] +=1;
-//             diff[currRem] = max(diff[currRem],1 + diff[1^currRem]);
-//         }
-//         return max({same[0], same[1], diff[0], diff[1]});
-//     }
-// };
 class Solution {
 public:
     int maximumLength(vector<int>& nums) {
-        int res = 0, n = nums.size();
-
-        int odd = 0, even = 0, alternate = 1, prev = nums[0];
+        vector<int> same(2,0) , diff(2,0);
+        int n = nums.size();
         for(int i = 0; i < n; i++){
-            if(nums[i] % 2 == 1) odd++;
-            else even++;
-
-            if(i >= 1 && nums[i] % 2 != prev % 2){
-                alternate++;
-                prev = nums[i];
-            }
+            int currRem = nums[i]%2;
+            same[currRem] +=1;
+            diff[currRem] = max(diff[currRem],1 + diff[1^currRem]);
         }
-        return max({odd, even, alternate});
+        return max({same[0], same[1], diff[0], diff[1]});
     }
 };
