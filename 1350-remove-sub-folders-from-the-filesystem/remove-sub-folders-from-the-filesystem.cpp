56a74fc553fbbb31910de96646b48b2401f3ecb2
pg1305 <135537396+pg1305@users.noreply.github.com>
2025-07-19 11:16:00 +0530

    Time: 55 ms (66.88%) | Memory: 49.2 MB (74.62%) - LeetSync

--- 1350-remove-sub-folders-from-the-filesystem/remove-sub-folders-from-the-filesystem.cpp
+++ 1350-remove-sub-folders-from-the-filesystem/remove-sub-folders-from-the-filesystem.cpp
@@ -4,9 +4,10 @@ public:
         sort(folder.begin(), folder.end());
         vector<string> result;
         for(const string&path : folder){
-            if(result.empty() || path.find(result.back() + '/') != 0){
-                result.push_back(path);
-            }
+        if (result.empty() || path.substr(0, result.back().size() + 1) != result.back() + "/") {
+            result.push_back(path);
+        }
+
         }
         return result;
     }
