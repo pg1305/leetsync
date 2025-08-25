4142a97747fd6300bfb43d08e873697a73807d22
pg1305 <135537396+pg1305@users.noreply.github.com>
2025-08-26 00:24:39 +0530

    Time: 0 ms (100.00%) | Memory: 25.1 MB (86.73%) - LeetSync

--- 645-set-mismatch/set-mismatch.cpp
+++ 645-set-mismatch/set-mismatch.cpp
@@ -1,25 +1,24 @@
 class Solution {
 public:
     vector<int> findErrorNums(vector<int>& nums) {
-        vector<int> count(nums.size()+1 , 0);
-        vector<int> ans;
+    int n = nums.size();
 
-        for(int i=0;i<nums.size();i++){
-            count[nums[i]]++;
-        }
+    long long S_actual = 1LL * n * (n + 1) / 2;
+    long long Q_actual = 1LL * n * (n + 1) * (2LL * n + 1) / 6;
 
-        for(int i=1;i<count.size();i++){
-            if(count[i] == 2){
-                ans.push_back(i);
-            }
-        }
-
-        for(int i=1;i<count.size();i++){
-            if(count[i] == 0){
-                ans.push_back(i);
-            }
-        }
-
-        return ans;
+    long long S = 0, Q = 0;
+    for (int x : nums) {
+        S += x;
+        Q += 1LL * x * x;
     }
+
+    long long diff1 = S - S_actual;       // A - B
+    long long diff2 = Q - Q_actual;       // A^2 - B^2
+
+    long long sumAB = diff2 / diff1;      // A + B
+    int A = (diff1 + sumAB) / 2;
+    int B = A - diff1;
+
+    return {A, B};
+}
 };
