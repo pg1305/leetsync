e2c0d7645eb9ac1feb5d068208947654d199a813
pg1305 <135537396+pg1305@users.noreply.github.com>
2025-09-04 00:01:00 +0530

    Time: 97 ms (97.87%) | Memory: 43.4 MB (17.02%) - LeetSync

--- 3277-find-the-number-of-ways-to-place-people-ii/find-the-number-of-ways-to-place-people-ii.cpp
+++ 3277-find-the-number-of-ways-to-place-people-ii/find-the-number-of-ways-to-place-people-ii.cpp
@@ -1,26 +1,22 @@
 class Solution {
+
 public:
     int numberOfPairs(vector<vector<int>>& points) {
+        int n = points.size();
+        vector<pair<int, int>> vec;
+        for (auto& p:points) {
+            vec.push_back({p[0], -p[1]});
+        }
+        ranges::sort(vec);
         int ans = 0;
-        sort(points.begin(), points.end(),
-             [](const vector<int>& a, const vector<int>& b) {
-                 return a[0] < b[0] || (a[0] == b[0] && a[1] > b[1]);
-             });
-
-        for (int i = 0; i < points.size() - 1; i++) {
-            const auto& pointA = points[i];
-            int xMin = pointA[0] - 1;
-            int xMax = INT_MAX;
-            int yMin = INT_MIN;
-            int yMax = pointA[1] + 1;
-
-            for (int j = i + 1; j < points.size(); j++) {
-                const auto& pointB = points[j];
-                if (pointB[0] > xMin && pointB[0] < xMax && pointB[1] > yMin &&
-                    pointB[1] < yMax) {
+        for (int i = 0; i < n; i++) {
+            int yi = -vec[i].second;
+            int maxyj = INT_MIN;
+            for (int j = i + 1; j < n; j++) {
+                int yj = -vec[j].second;
+                if (yj <= yi && yj > maxyj) {
+                    maxyj = yj;
                     ans++;
-                    xMin = pointB[0];
-                    yMin = pointB[1];
                 }
             }
         }
