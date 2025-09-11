5f162b6bb934e248eb2e8755cb3a834a3507da2b
pg1305 <135537396+pg1305@users.noreply.github.com>
2025-09-11 11:30:46 +0530

    Time: 16 ms (49.09%) | Memory: 16.7 MB (33.07%) - LeetSync

--- 2887-sort-vowels-in-a-string/sort-vowels-in-a-string.cpp
+++ 2887-sort-vowels-in-a-string/sort-vowels-in-a-string.cpp
@@ -1,22 +1,41 @@
-class Solution { 
-    bool isVowel(char c){
-        return c == 'a' || c == 'e' ||c == 'i' ||c == 'o' ||c == 'u' ||c == 'A' ||c == 'E' ||c == 'I' ||c == 'O' ||c == 'U';
-    }
+class Solution {
 public:
     string sortVowels(string s) {
-        string vowels;
-        for(char c : s){
-            if(isVowel(c)) vowels += c;
+        vector<int> lower(26,0),upper(26,0);
+        int n=s.size();
+        for(int i=0;i<n;i++){
+            if(s[i]=='a' || s[i]=='e' || s[i]=='i' || s[i]=='o' || s[i]=='u'){
+                lower[s[i]-'a']++;
+                s[i]='0';
+            }
+            if(s[i]=='A' || s[i]=='E' || s[i]=='I' || s[i]=='O' || s[i]=='U'){
+                upper[s[i]-'A']++;
+                s[i]='0';
+            }
         }
-        sort(vowels.begin(), vowels.end());
-        int k = 0;
-        string res;
-        for(char c : s){
-            if(isVowel(c)){
-                res += vowels[k];
-                k++;
-            }else res += c;
+        string ans;
+        for(int i=0;i<26;i++){
+            char c='A'+i;
+            while(upper[i]){
+                ans+=c;
+                upper[i]--;
+            }
         }
-        return res;
+        for(int i=0;i<26;i++){
+            char c='a'+i;
+            while(lower[i]){
+                ans+=c;
+                lower[i]--;
+            }
+        }
+        int first = 0,second= 0;
+        while(second<ans.size()){
+            if(s[first]=='0'){
+                s[first]=ans[second];
+                second++;
+            }
+            first++;
+        }
+        return s;
     }
 };
