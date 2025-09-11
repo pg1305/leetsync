237baf9faa3cbff28954b0ac1ece91e0cc09c89e
pg1305 <135537396+pg1305@users.noreply.github.com>
2025-09-11 11:31:17 +0530

    Time: 6 ms (92.06%) | Memory: 16.3 MB (64.61%) - LeetSync

--- 2887-sort-vowels-in-a-string/sort-vowels-in-a-string.cpp
+++ 2887-sort-vowels-in-a-string/sort-vowels-in-a-string.cpp
@@ -1,41 +1,43 @@
 class Solution {
 public:
     string sortVowels(string s) {
-        vector<int> lower(26,0),upper(26,0);
-        int n=s.size();
-        for(int i=0;i<n;i++){
-            if(s[i]=='a' || s[i]=='e' || s[i]=='i' || s[i]=='o' || s[i]=='u'){
+        vector<int>lower(26,0);
+        vector<int>upper(26,0);
+        
+        for(int i = 0; i<s.size(); i++){
+            if(s[i]=='a'|| s[i]=='e'|| s[i]=='i'|| s[i]=='o'|| s[i]=='u'){
                 lower[s[i]-'a']++;
-                s[i]='0';
+                s[i]='#';
             }
-            if(s[i]=='A' || s[i]=='E' || s[i]=='I' || s[i]=='O' || s[i]=='U'){
+            else if(s[i]=='A'|| s[i]=='E'|| s[i]=='I'|| s[i]=='O'|| s[i]=='U'){
                 upper[s[i]-'A']++;
-                s[i]='0';
+                s[i]='#';
             }
         }
-        string ans;
-        for(int i=0;i<26;i++){
-            char c='A'+i;
+        string v;
+        for(int i = 0; i < 26; i++){
+            char c = 'A' + i;
             while(upper[i]){
-                ans+=c;
+                v += c;
                 upper[i]--;
             }
         }
-        for(int i=0;i<26;i++){
-            char c='a'+i;
+        for(int i = 0; i < 26; i++){
+            char c = 'a' + i;
             while(lower[i]){
-                ans+=c;
+                v += c;
                 lower[i]--;
             }
         }
-        int first = 0,second= 0;
-        while(second<ans.size()){
-            if(s[first]=='0'){
-                s[first]=ans[second];
-                second++;
+    
+        int second = 0;
+        for (int i = 0; i < s.size(); i++){
+            if(s[i]=='#'){
+                s[i]=v[second];
+                second++;          
             }
-            first++;
         }
         return s;
+
     }
 };
