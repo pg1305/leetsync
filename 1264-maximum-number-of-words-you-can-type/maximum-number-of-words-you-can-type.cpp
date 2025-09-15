fc2cfcc25be45a3d2440ec80dfc899fb7a8b225a
pg1305 <135537396+pg1305@users.noreply.github.com>
2025-09-16 00:38:28 +0530

    Time: 0 ms (100.00%) | Memory: 10.2 MB (5.65%) - LeetSync

--- 1264-maximum-number-of-words-you-can-type/maximum-number-of-words-you-can-type.cpp
+++ 1264-maximum-number-of-words-you-can-type/maximum-number-of-words-you-can-type.cpp
@@ -1,20 +1,28 @@
 class Solution {
 public:
-    int canBeTypedWords(string text, string brokenLetters) {
-        unordered_set<char> broken(brokenLetters.begin(), brokenLetters.end());
-        int words = 0;
-        bool canType = true;
-        for (char c : text) {
-            if (c == ' ') {
-                if (canType) words++;
-                canType = true; 
-            } else {
-                if (broken.count(c)) {
-                    canType = false;  
+    int canBeTypedWords(string text, string brokenLetters){
+        unordered_set<char> st;
+        int cnt = 0;
+        for(auto& it : brokenLetters){
+            st.insert(it);
+        }
+        stringstream ss(text);
+        string word;
+        vector<string> words;
+        while(ss >> word){
+            words.push_back(word);
+        }
+        for(auto w: words)
+        {
+            for(char c : w)
+            {
+                if(st.find(c) != st.end())
+                {
+                    cnt++;
+                    break;
                 }
             }
         }
-        if (canType) words++;
-        return words;
+        return words.size() - cnt;
     }
 };
