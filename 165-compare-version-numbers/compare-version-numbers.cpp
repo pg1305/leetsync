90cac9f3d17ada9b60745fe1e4c5911c0d3dd124
pg1305 <135537396+pg1305@users.noreply.github.com>
2025-09-24 00:59:10 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (48.40%) - LeetSync

--- 165-compare-version-numbers/compare-version-numbers.cpp
+++ 165-compare-version-numbers/compare-version-numbers.cpp
@@ -3,7 +3,6 @@ public:
     int compareVersion(string version1, string version2) {
         int n1 = version1.size(), n2 = version2.size();
         int i = 0, j = 0;
-
         while (i < n1 || j < n2) {
             int num1 = 0, num2 = 0;
             while (i < n1 && version1[i] != '.') {
@@ -14,14 +13,11 @@ public:
                 num2 = num2 * 10 + (version2[j] - '0');
                 j++;
             }
-
             if (num1 > num2) return 1;
             if (num1 < num2) return -1;
-
             i++; 
             j++; 
         }
-
         return 0;
     }
 };
