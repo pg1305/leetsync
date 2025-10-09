fc5f1d900b9bba2d5600dfd62cbc5931bbe43f30
pg1305 <135537396+pg1305@users.noreply.github.com>
2025-10-09 22:31:53 +0530

    Time: 11 ms (29.05%) | Memory: 127.6 MB (86.49%) - LeetSync

--- 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.cpp
+++ 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.cpp
@@ -1,17 +1,30 @@
 class Solution {
 public:
     vector<int> pivotArray(vector<int>& nums, int pivot) {
-        vector<int> less, great, equal;
+        // vector<int> less, great, equal;
+        // for(int num:nums){
+        //     if(num < pivot) less.push_back(num);
+        //     else if(num == pivot) equal.push_back(num);
+        //     else great.push_back(num);
+        // }
+        // vector<int> res;
+        // res.reserve(nums.size());
+        // res.insert(res.end(), less.begin(), less.end());
+        // res.insert(res.end(), equal.begin(), equal.end());
+        // res.insert(res.end(), great.begin(), great.end());
+        // return res;
+        vector<int> res(nums.size());
+        int less = 0, equal = 0;
         for(int num:nums){
-            if(num < pivot) less.push_back(num);
-            else if(num == pivot) equal.push_back(num);
-            else great.push_back(num);
+            if(num < pivot) less++;
+            else if(num == pivot) equal++;
         }
-        vector<int> res;
-        res.reserve(nums.size());
-        res.insert(res.end(), less.begin(), less.end());
-        res.insert(res.end(), equal.begin(), equal.end());
-        res.insert(res.end(), great.begin(), great.end());
+        int i1 = 0, i2 = less, i3 = less + equal;
+        for(int num:nums){
+            if(num < pivot) res[i1++] = num;
+            else if(num == pivot) res[i2++] = num;
+            else res[i3++] = num;
+        }
         return res;
     }
 };
