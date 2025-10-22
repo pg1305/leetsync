2c8c3bfd52516ee0ecde0516703b7bb7ca518c17
pg1305 <135537396+pg1305@users.noreply.github.com>
2025-10-22 10:05:06 +0530

    Time: 3 ms (46.34%) | Memory: 36.6 MB (63.09%) - LeetSync

--- 1428-jump-game-iii/jump-game-iii.cpp
+++ 1428-jump-game-iii/jump-game-iii.cpp
@@ -1,27 +1,23 @@
 class Solution {
 public:
-    bool p = false;
-    void dfs(vector<vector<int>>& adj, vector<bool>& visited, int start, vector<int>& arr){
-        visited[start] = true;
-        if(arr[start] == 0){
-            p = true;
-            return;
-        }
-        for(auto u:adj[start]){
-            if(visited[u] == false) dfs(adj, visited, u, arr);
-        }
-    }
     bool canReach(vector<int>& arr, int start) {
         int n = arr.size();
-        vector<vector<int>> adj(n + 10);
-        for(int i = 0; i < n; i++){
-            int x = arr[i] + i;
-            int y = i - arr[i];
-            if(x < n) adj[i].push_back(x);
-            if(y >= 0) adj[i].push_back(y);
+        queue<int> q;
+        vector<int> vis(n, false);
+        q.emplace(start);
+        vis[start] = true;
+        while(!q.empty()) {
+            auto u = q.front();
+            q.pop();
+            for(const auto& v : {u + arr[u], u - arr[u]}) {
+                if(v >= n || v < 0 || vis[v])
+                    continue;
+                if(arr[v] == 0)
+                    return true;
+                vis[v] = true;
+                q.emplace(v);
+            }
         }
-        vector<bool>visited(n + 10, false);
-        dfs(adj, visited, start, arr);
-        return p;
+        return arr[start] == 0;
     }
 };
