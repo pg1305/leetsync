1c1afe2d3dee6ec049b6d556d72594b8d8e7e285
pg1305 <135537396+pg1305@users.noreply.github.com>
2025-10-29 14:54:35 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (89.59%) - LeetSync

--- 3676-smallest-number-with-all-set-bits/smallest-number-with-all-set-bits.cpp
+++ 3676-smallest-number-with-all-set-bits/smallest-number-with-all-set-bits.cpp
@@ -1,14 +1,10 @@
 class Solution {
 public:
     int smallestNumber(int n) {
-        string bin = "";
-        int i = n;
-        while(i > 0){
-            int bit = i % 2;
-            bin += to_string(bit);
-            i /= 2;
+        int x = 1;
+        while (x < n) {
+            x = x * 2 + 1;
         }
-        int k = bin.size();
-        return pow(2,k) - 1;
+        return x;
     }
 };
