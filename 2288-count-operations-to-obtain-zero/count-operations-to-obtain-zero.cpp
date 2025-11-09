5423029714682d196b23a47b061a2700af48cc8a
pg1305 <135537396+pg1305@users.noreply.github.com>
2025-11-10 00:31:41 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (71.74%) - LeetSync

--- 2288-count-operations-to-obtain-zero/count-operations-to-obtain-zero.cpp
+++ 2288-count-operations-to-obtain-zero/count-operations-to-obtain-zero.cpp
@@ -1,12 +1,16 @@
 class Solution {
 public:
     int countOperations(int num1, int num2) {
-       int res = 0;
-       while(num1 && num2){
-        res += num1/num2;
-        num1 %= num2;
-        swap(num1, num2);
-       } 
-       return res;
+        long long count = 0;
+        while(num1!=0 && num2!=0){
+            if(num1>=num2){
+                num1 = num1-num2;
+            }
+            else{
+                num2 = num2-num1;
+            }
+            count++;
+        }
+        return count;
     }
 };
