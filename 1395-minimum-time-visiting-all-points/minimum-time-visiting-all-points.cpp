d790331cc674ea1c423698d8f1c4926ddf84e6f0
pg1305 <135537396+pg1305@users.noreply.github.com>
2026-01-12 15:04:59 +0530

    Time: 0 ms (100.00%) | Memory: 13.9 MB (26.76%) - LeetSync

--- 1395-minimum-time-visiting-all-points/minimum-time-visiting-all-points.cpp
+++ 1395-minimum-time-visiting-all-points/minimum-time-visiting-all-points.cpp
@@ -1,14 +1,24 @@
 class Solution {
 public:
     int minTimeToVisitAllPoints(vector<vector<int>>& points) {
-        int res = 0;
-        for (int i = 0; i < points.size() - 1; i++) {
-            int X = points[i][0];
-            int Y = points[i][1];
-            int targetX = points[i + 1][0];
-            int targetY = points[i + 1][1];
-            res += max(abs(targetX - X), abs(targetY - Y));
+
+        int n = points.size();
+        int steps = 0;
+
+        for(int i=0;i<n-1;i++){
+
+            int x1 = points[i][0];
+            int y1 = points[i][1];
+
+            int x2 = points[i+1][0];
+            int y2 = points[i+1][1];
+
+            int dy = abs(y2-y1);
+            int dx = abs(x2-x1);
+
+            steps = steps + (min(dx,dy) + abs(dy-dx));
         }
-        return res;
+        return steps;
+        
     }
 };
