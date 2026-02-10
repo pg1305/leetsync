dbeb076827c1c4f4561c877e2a818c638adf1e4b
pg1305 <135537396+pg1305@users.noreply.github.com>
2026-02-10 23:15:07 +0530

    Time: 1709 ms (82.18%) | Memory: 426.8 MB (88.47%) - LeetSync

--- 4045-longest-balanced-subarray-i/longest-balanced-subarray-i.cpp
+++ 4045-longest-balanced-subarray-i/longest-balanced-subarray-i.cpp
@@ -1,16 +1,18 @@
 class Solution {
 public:
     int longestBalanced(vector<int>& nums) {
-        int length = 0;
-        for(int i = 0; i < nums.size(); i++){
-            auto odd = unordered_map<int, int> ();
-            auto even = unordered_map<int, int>();
-            for(int j = i; j < nums.size(); j++){
-                auto& c = (nums[j] & 1)? odd:even;
-                c[nums[j]]++;
-                if(odd.size() == even.size()) length = max(length, j - i + 1); 
-            }
+        
+        int maxi=0;
+        unordered_set<int>even,odd;
+        for(int i=0;i<nums.size();i++){
+            even.clear();
+            odd.clear();
+            for(int j=i;j<nums.size();j++){
+                if((nums[j]&1)==0)even.insert(nums[j]);
+                else odd.insert(nums[j]);
+            if(even.size()==odd.size())maxi=max(maxi,j-i+1);
+            }  
         }
-        return length;
+        return maxi;
     }
 };
