c68f037f4e5e0d43d61f1d01ab34dcb0094e64cd
pg1305 <135537396+pg1305@users.noreply.github.com>
2026-03-01 00:31:19 +0530

    Time: 29 ms (67.11%) | Memory: 8.1 MB (69.80%) - LeetSync

---
+++ 1800-concatenation-of-consecutive-binary-numbers/concatenation-of-consecutive-binary-numbers.cpp
@@ -0,0 +1,13 @@
+class Solution {
+    const int mod = 1e9 + 7;
+public:
+    int concatenatedBinary(int n) {
+        long long res = 0;
+        int bits = 0;
+        for(int i = 1; i <= n; i++){
+            if((i & (i - 1)) == 0) bits++;
+            res = ((res << bits) | i) % mod;
+        }
+        return res;
+    }
+};
