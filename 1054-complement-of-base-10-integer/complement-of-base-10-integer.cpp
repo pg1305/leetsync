c340fa121cc25b201fc48cf6a5695cc5f6688166
pg1305 <135537396+pg1305@users.noreply.github.com>
2026-03-11 14:27:40 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (90.31%) - LeetSync

---
+++ 1054-complement-of-base-10-integer/complement-of-base-10-integer.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int bitwiseComplement(int n) {
+        if(n == 0) return 1;
+        int mask = 0;
+        int temp = n;
+        while(temp){
+            mask = (mask << 1) | 1;
+            temp >>= 1;
+        }
+        return mask ^ n;
+    }
+};
