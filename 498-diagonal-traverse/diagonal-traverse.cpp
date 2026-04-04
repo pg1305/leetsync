f7007a9201a6e59815490e6143413d0ec7988b0d
pg1305 <135537396+pg1305@users.noreply.github.com>
2026-04-04 09:45:59 +0530

    Time: 0 ms (100.00%) | Memory: 22.4 MB (91.68%) - LeetSync

---
+++ 498-diagonal-traverse/diagonal-traverse.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> findDiagonalOrder(vector<vector<int>>& mat) {
+        if(mat.empty() || mat[0].empty()) return {};
+        int m = mat.size(), n = mat[0].size();
+        vector<int> res(m*n);
+        int r = 0, c = 0;
+        for(int i = 0; i < m*n; i++){
+            res[i] = mat[r][c];
+            if((r + c) % 2 == 0){
+                if(c == n - 1) r++;
+                else if(r == 0) c++;
+                else {r--; c++;}
+            }else{
+                if(r == m - 1) c++;
+                else if(c == 0) r++;
+                else{ r++; c--;}
+            }
+        }
+        return res;
+    }
+};
