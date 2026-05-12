2f36a569c2a5b8c85d34303801deca7b5d2b1bf8
pg1305 <135537396+pg1305@users.noreply.github.com>
2026-05-12 15:09:46 +0530

    Time: 83 ms (27.67%) | Memory: 110.5 MB (22.82%) - LeetSync

---
+++ 1784-minimum-initial-energy-to-finish-tasks/minimum-initial-energy-to-finish-tasks.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int minimumEffort(vector<vector<int>>& tasks) {
+        sort(tasks.begin(), tasks.end(), [&](vector<int>& a, vector<int>& b){
+            return a[1] - a[0] < b[1] - b[0];
+        });
+        int res = 0;
+        for(auto t: tasks) res = max(res + t[0], t[1]);
+        return res;
+    }
+};
