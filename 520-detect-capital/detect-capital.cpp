1adf5d506a91fcb37447d554e4f24cfbd9a9d77f
pg1305 <135537396+pg1305@users.noreply.github.com>
2026-05-29 01:11:02 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (16.91%) - LeetSync

---
+++ 520-detect-capital/detect-capital.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool detectCapitalUse(string word) {
+        int count = 0;
+        for(int i = 0 ; i < word.length() ; i++){
+            if(isupper(word[i])) count++;
+        }
+        if(count == word.length()) return true;
+        else if(isupper(word[0]) && count == 1){
+            return true;
+        }
+        else if(count == 0){
+            return true;
+        }
+        else return false;
+    }
+};
