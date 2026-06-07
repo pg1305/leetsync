66088eae3692524ee957075758b84ac9c50aadd3
pg1305 <135537396+pg1305@users.noreply.github.com>
2026-06-08 00:50:13 +0530

    Time: 179 ms (78.49%) | Memory: 279.1 MB (59.54%) - LeetSync

---
+++ 2306-create-binary-tree-from-descriptions/create-binary-tree-from-descriptions.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    TreeNode* createBinaryTree(vector<vector<int>>& descriptions) {
+        unordered_map<int, TreeNode*> nodeMap;
+        unordered_set<int> children;
+        for (const auto& description : descriptions) {
+            int parentValue = description[0];
+            int childValue = description[1];
+            bool isLeft = description[2];
+            if (nodeMap.count(parentValue) == 0) {
+                nodeMap[parentValue] = new TreeNode(parentValue);
+            }
+            if (nodeMap.count(childValue) == 0) {
+                nodeMap[childValue] = new TreeNode(childValue);
+            }
+            if (isLeft) {
+                nodeMap[parentValue]->left = nodeMap[childValue];
+            } else {
+                nodeMap[parentValue]->right = nodeMap[childValue];
+            }
+            children.insert(childValue);
+        }
+        for (auto& entry : nodeMap) {
+            auto& value = entry.first;
+            auto& node = entry.second;
+            if (children.find(value) == children.end()) {
+                return node;
+            }
+        }
+        return nullptr;
+    }
+};
