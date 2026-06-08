7a02df51db230c4509dbe7a28efe1f958befb50c
pg1305 <135537396+pg1305@users.noreply.github.com>
2026-06-08 22:20:31 +0530

    Time: 8 ms (51.49%) | Memory: 129.6 MB (70.73%) - LeetSync

--- 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.cpp
+++ 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.cpp
@@ -1,30 +1,22 @@
 class Solution {
 public:
     vector<int> pivotArray(vector<int>& nums, int pivot) {
-        
-        std::vector<int> G,L;
+        vector<int> G,L;
         G.reserve(nums.size());
         L.reserve(nums.size());
         int NPivot=0;
         for(int i=0;i<nums.size();i++)
         {
-            if(nums[i]<pivot)
-            {
-                L.emplace_back(nums[i]);
-            }
-            else if(nums[i] == pivot){
-                NPivot++;
-            }
-            else{
-                G.emplace_back(nums[i]);
-            }
+            if(nums[i]<pivot) L.emplace_back(nums[i]);
+            else if(nums[i] == pivot) NPivot++;
+            else G.emplace_back(nums[i]);
+            
         }
-
         while(NPivot>0){
             L.emplace_back(pivot);
             NPivot--;
         }
-        L.insert(L.end(),std::make_move_iterator(G.begin()),std::make_move_iterator(G.end()) );
-        return std::move(L);
+        L.insert(L.end(),make_move_iterator(G.begin()),make_move_iterator(G.end()) );
+        return move(L);
     }
 };
