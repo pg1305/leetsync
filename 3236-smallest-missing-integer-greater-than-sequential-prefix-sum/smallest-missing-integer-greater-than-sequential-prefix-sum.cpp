f053a7afa14dc0728c7567bc384491303df97e7d
pg1305 <135537396+pg1305@users.noreply.github.com>
2026-08-11 14:31:22 +0530

    Time: 0 ms (100.00%) | Memory: 22.5 MB (90.34%) - LeetSync

--- 3236-smallest-missing-integer-greater-than-sequential-prefix-sum/smallest-missing-integer-greater-than-sequential-prefix-sum.cpp
+++ 3236-smallest-missing-integer-greater-than-sequential-prefix-sum/smallest-missing-integer-greater-than-sequential-prefix-sum.cpp
@@ -2,16 +2,30 @@ class Solution {
 public:
     int missingInteger(vector<int>& nums) {
         int sum = nums[0];
-        for (int i = 1; i < nums.size(); i++) {
-            if (nums[i] == nums[i - 1] + 1)
-                sum += nums[i];
-            else
+        for(int i = 1; i < nums.size(); i++){
+            if(nums[i-1] + 1 == nums[i]){
+                sum = sum + nums[i];
+            }
+            else {
                 break;
+            }
         }
-        unordered_set<int> st(nums.begin(), nums.end());
-        while (st.count(sum)) {
-            sum++;
+        
+        sort(nums.begin(), nums.end());
+        
+        while(true){
+            bool found = false;
+            for(int i = nums.size() - 1; i >= 0; i--){
+                if(sum == nums[i]){
+                    sum = sum + 1;
+                    found = true;
+                    break; 
+                }
+            }
+            
+            if (!found) {
+                return sum;
+            }
         }
-        return sum;
     }
 };
