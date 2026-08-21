a63ef31fb71447d76fb70885c147ea4f0111dee7
pg1305 <135537396+pg1305@users.noreply.github.com>
2026-08-21 19:25:53 +0530

    Time: 578 ms (13.42%) | Memory: 19.7 MB (48.78%) - LeetSync

---
+++ 3375-kth-smallest-amount-with-single-denomination-combination/kth-smallest-amount-with-single-denomination-combination.py
@@ -0,0 +1,9 @@
+class Solution:
+    def findKthSmallest(self, A: List[int], k: int) -> int:
+        def check(m):
+            tot = 0
+            for x in range(1, len(A) + 1):
+                for c in combinations(A, x):
+                    tot += m // lcm(*c) * pow(-1, x + 1)
+            return tot >= k
+        return bisect_left(range(k * A[0] + 1), True, lo=1, key=check)
