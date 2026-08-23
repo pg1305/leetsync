ff0689ae41dd02ef1f22d91f9df50e9ff1b27c8f
pg1305 <135537396+pg1305@users.noreply.github.com>
2026-08-23 17:15:26 +0530

    Time: 3 ms (66.67%) | Memory: 14.1 MB (10.26%) - LeetSync

--- 2039-sum-game/sum-game.cpp
+++ 2039-sum-game/sum-game.cpp
@@ -1,21 +1,41 @@
 class Solution {
 public:
-    bool sumGame(string num) {
-        int n = num.size();
-        int half = n / 2;
-        int sumL = 0, sumR = 0;
-        int countL = 0, countR = 0;
-        for(int i = 0; i < half; i++){
-            if(num[i] == '?') countL++;
-            else sumL += num[i] - '0';
+    bool sumGame(string s) {
+        int n=s.length();
+        int h=n>>1;
+        int L=0,R=0;
+        int lq=0,rq=0;
+        for(int i=0;i<n;++i){
+            if(i<h){
+                if(s[i]=='?')
+                    ++lq;
+                else
+                    L+=s[i]-'0';
+            }else{
+                if(s[i]=='?')
+                    ++rq;
+                else
+                    R+=s[i]-'0';
+            }
         }
-        for(int i = half; i < n; i++){
-            if(num[i] == '?') countR++;
-            else sumR += num[i] - '0';
+        if(lq<rq){
+            swap(L,R);
+            swap(lq,rq);
         }
-        if((countL + countR) % 2 == 1) return true;
-        int del = sumL - sumR;
-        int delQ = countR - countL;
-        return del*2 != delQ*9;
+        int rem=lq-rq;
+        if(rem==0)
+            return L!=R;
+        // println("{} {} {}",L,R,rem);
+        if(L>=R)
+            return true;
+        int da=(rem+1)>>1;
+        int db=rem>>1;
+        int alice_max=L+da*9;
+        if(alice_max>R)
+            return true;
+        int bob_max=L+db*9;
+        // println("{} {}",alice_max,bob_max);
+        //L<R
+        return bob_max<R;
     }
 };
