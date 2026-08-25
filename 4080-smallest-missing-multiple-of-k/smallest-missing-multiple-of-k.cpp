c697c67e0fc4f7950d53807630d15239ba5edfef
pg1305 <135537396+pg1305@users.noreply.github.com>
2026-08-25 23:39:24 +0530

    Time: 0 ms (100.00%) | Memory: 24 MB (94.59%) - LeetSync

--- 4080-smallest-missing-multiple-of-k/smallest-missing-multiple-of-k.cpp
+++ 4080-smallest-missing-multiple-of-k/smallest-missing-multiple-of-k.cpp
@@ -1,9 +1,17 @@
 class Solution {
 public:
     int missingMultiple(vector<int>& nums, int k) {
-        unordered_set<int> seen(nums.begin(), nums.end());
-        int res = k;
-        while(seen.count(res)) res+= k;
-        return res;
+        vector<bool> freq(101,0);
+
+        for(int i=0;i<nums.size();i++)
+        freq[nums[i]] = 1;
+
+        int i = k;
+        while(i <= 100) {
+            if(!freq[i])
+            return i;
+            i += k;
+        }
+        return ((100 / k) + 1) * k;
     }
 };
