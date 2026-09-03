f8f4bb3bca1aba23f67dd219970420e7149d3157
pg1305 <135537396+pg1305@users.noreply.github.com>
2026-09-03 10:50:26 +0530

    Time: 0 ms (100.00%) | Memory: 166 MB (18.85%) - LeetSync

--- 4258-construct-uniform-parity-array-ii/construct-uniform-parity-array-ii.cpp
+++ 4258-construct-uniform-parity-array-ii/construct-uniform-parity-array-ii.cpp
@@ -1,13 +1,16 @@
 class Solution {
 public:
     bool uniformArray(vector<int>& nums1) {
-        int min = *min_element(nums1.begin(), nums1.end());
-        if(min % 2 == 1) return true;
-        else{
-            for(int num:nums1){
-                if(num % 2 == 1) return false;
+        int n = nums1.size();
+        int min_odd = 1e9 + 7;
+        for (int i : nums1) if (i & 1) min_odd = min(min_odd, i);
+        
+        auto helper = [&](int target_p) {
+            for (int i : nums1) {
+                if (i % 2 != target_p && i <= min_odd) return false;
             }
-        }
-        return true;
+            return true;
+        };
+        return helper(0) || helper(1);
     }
 };
