16b432007afd8d88d6c76e94f1b4eb996d51682b
pg1305 <135537396+pg1305@users.noreply.github.com>
2026-09-14 23:59:31 +0530

    Time: 0 ms (100.00%) | Memory: 10.5 MB (39.67%) - LeetSync

--- 223-rectangle-area/rectangle-area.cpp
+++ 223-rectangle-area/rectangle-area.cpp
@@ -1,15 +1,43 @@
 class Solution {
 public:
     int computeArea(int ax1, int ay1, int ax2, int ay2, int bx1, int by1, int bx2, int by2) {
-        int rectW1 = abs(ax2 - ax1);
-        int rectW2 = abs(bx2 - bx1);
-        int rectL1 = abs(ay2 - ay1);
-        int rectL2 = abs(by2 - by1);
-        int ar1 = rectL1 * rectW1;
-        int ar2 = rectL2 * rectW2;
-        int comL = max(0 , min(ax2, bx2) - max(ax1, bx1));
-        int comW = max(0, min(ay2, by2) - max(ay1, by1));
-        int comAr = comL * comW;
-        return ar1 + ar2 - comAr;
+        int tx1 = -1, tx2 = -1, ty1 = -1, ty2 = -1;
+        if(ax1 >= bx1 && ax1 < bx2){
+            tx1 = ax1;
+        }
+
+        if(bx1 >= ax1 && bx1 < ax2){
+            tx1 = bx1;
+        }
+
+        if(ax2 <= bx2 && ax2 > bx1){
+            tx2 = ax2;
+        }
+
+        if(bx2 <= ax2 && bx2 > ax1){
+            tx2 = bx2;
+        }
+
+        if(ay1 >= by1 && ay1 < by2){
+            ty1 = ay1;
+        }
+
+        if(by1 >= ay1 && by1 < ay2){
+            ty1 = by1;
+        }
+
+        if(ay2 <= by2 && ay2 > by1){
+            ty2 = ay2;
+        }
+
+        if(by2 <= ay2 && by2 > ay1){
+            ty2 = by2;
+        }
+        int areaA = (ax2- ax1) * (ay2 - ay1);
+        int areaB = (bx2- bx1) * (by2 - by1);
+        int areaT = (tx2- tx1) * (ty2 - ty1);
+
+        return areaA + areaB - areaT;
+        
     }
 };
