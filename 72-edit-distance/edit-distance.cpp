9086ed4b8ee2af0afe5995a02abbffc26c360209
pg1305 <135537396+pg1305@users.noreply.github.com>
2026-09-15 22:01:45 +0530

    Time: 7 ms (61.34%) | Memory: 14.4 MB (15.22%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -1,24 +1,26 @@
 class Solution {
- public:
-  int minDistance(string word1, string word2) {
-    const int m = word1.length();//first word length
-    const int n = word2.length();//second word length
-    // dp[i][j] := min # of operations to convert word1[0..i) to word2[0..j)
-    vector<vector<int>> dp(m + 1, vector<int>(n + 1));
+public:
+    int solve(string &s , string &t , vector<vector<int>>&dp , int i , int j)
+    {
+        if(j<0) return i+1;
+        if(i<0) return j+1;
+        if(dp[i][j]!=-1) return dp[i][j];
 
-    for (int i = 1; i <= m; ++i)
-      dp[i][0] = i;
+        if(s[i]==t[j])
+        {
+           return dp[i][j] =solve(s,t,dp,i-1,j-1);
+        }
+        int ins = 1+solve(s ,t , dp , i , j-1);
+        int del = 1+solve(s ,t , dp , i-1 , j);
+        int rep = 1+solve(s ,t , dp , i-1 , j-1);
 
-    for (int j = 1; j <= n; ++j)
-      dp[0][j] = j;
+        return dp[i][j]=min({ins , del , rep});
 
-    for (int i = 1; i <= m; ++i)
-      for (int j = 1; j <= n; ++j)
-        if (word1[i - 1] == word2[j - 1])//same characters
-          dp[i][j] = dp[i - 1][j - 1];//no operation
-        else
-          dp[i][j] = min({dp[i - 1][j - 1], dp[i - 1][j], dp[i][j - 1]}) + 1;
-                             //replace       //delete        //insert
-    return dp[m][n];
-  }
+    }
+    int minDistance(string word1, string word2) {
+        int n = word1.size();
+        int m = word2.size();
+        vector<vector<int>>dp(n , vector<int>(m , -1));
+        return solve(word1,word2 , dp , n-1 , m-1);
+    }
 };
